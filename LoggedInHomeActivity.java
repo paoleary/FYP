81ad29be7356c35db31e6507ab237a870b64173f
paoleary <patrickoleary94@hotmail.com>
2018-04-11 00:18:34 +0100

    Final version

--- LoggedInHomeActivity.java
+++ LoggedInHomeActivity.java
@@ -1,21 +1,16 @@
 package com.example.pa.project2;
 
-import android.accessibilityservice.AccessibilityService;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.preference.DialogPreference;
-import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.pa.project2.model.Question;
@@ -24,8 +19,11 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.rengwuxian.materialedittext.MaterialEditText;
+
+import java.time.Month;
 
 public class LoggedInHomeActivity extends AppCompatActivity {
 
@@ -40,6 +38,7 @@ public class LoggedInHomeActivity extends AppCompatActivity {
     DatabaseReference quizzes;
     DatabaseReference quizQuestions;
     DatabaseReference questionAndAnswers;
+    DatabaseReference questions;
 
     //for creating new quiz
     MaterialEditText edtNewQuizName;
@@ -48,15 +47,25 @@ public class LoggedInHomeActivity extends AppCompatActivity {
     MaterialEditText edtNewAnswerB;
     MaterialEditText edtNewAnswerC;
     MaterialEditText edtNewAnswerD;
+    Spinner CorrectDropdown;
+    String correctAnswer;
     Boolean quizCreated = false;
     int quizCreatedInt = 0;
 
-    Spinner CorrectDropdown;
-    String correctAnswer;
+    //for taking quiz
+    MaterialEditText edtQuizName;
+    TextView textQuestion;
+    TextView textAnswerA;
+    TextView textAnswerB;
+    TextView textAnswerC;
+    TextView textAnswerD;
+    Spinner AnswerSelect;
+    String answerSelect;
+
 
 
     //for taking a quiz
-    MaterialEditText edtQuizName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +75,7 @@ public class LoggedInHomeActivity extends AppCompatActivity {
         //Firebase stuff
         database = FirebaseDatabase.getInstance();
         quizzes = database.getReference("Quizzes");
+
 
 
         btnAddQuiz = (Button)findViewById(R.id.btnAddQuiz);
@@ -91,6 +101,8 @@ public class LoggedInHomeActivity extends AppCompatActivity {
                 }
             }
         });
+
+
     }
 
     private void takeQuiz(final String quizName){
@@ -99,12 +111,101 @@ public class LoggedInHomeActivity extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 //check quizName field isn't empty
                 if(dataSnapshot.child(quizName).exists()){
-                    if(!quizName.isEmpty()){
-
+                    if(quizName.isEmpty()){
+                        Toast.makeText(LoggedInHomeActivity.this, "Please enter a quiz name", Toast.LENGTH_SHORT).show();
                     }
+                    else {
+                        final Quiz quiz = new Quiz(quizName);
+                        quizzes.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                //if(){
+                                //    Toast.makeText(LoggedInHomeActivity.this, "Quiz by that name does not exist", Toast.LENGTH_SHORT).show();
+                                //}
+                            }
 
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+
+
+                        final AlertDialog.Builder activeQuizDialog = new AlertDialog.Builder(LoggedInHomeActivity.this);
+                        activeQuizDialog.setMessage("Please select which answer you think is correct from the dropdown");
+
+                        final LayoutInflater takeQuizInflater = LayoutInflater.from(LoggedInHomeActivity.this);
+                        final View active_quiz_layout = takeQuizInflater.inflate(R.layout.active_quiz_layout, null);
+                        activeQuizDialog.setView(active_quiz_layout);
+
+                        textQuestion = active_quiz_layout.findViewById(R.id.textQuestion);
+                        textAnswerA = active_quiz_layout.findViewById(R.id.textAnswerA);
+                        textAnswerB = active_quiz_layout.findViewById(R.id.textAnswerB);
+                        textAnswerC = active_quiz_layout.findViewById(R.id.textAnswerC);
+                        textAnswerD = active_quiz_layout.findViewById(R.id.textAnswerD);
+
+                        //https://stackoverflow.com/questions/13377361/how-to-create-a-drop-down-list
+                        AnswerSelect = active_quiz_layout.findViewById(R.id.AnswerSelect);
+                        String[] items = new String[]{"A", "B", "C", "D"};
+                        //https://stackoverflow.com/questions/28848735/cannot-resolve-constructor-arrayadapter
+                        ArrayAdapter<String> adapter = new ArrayAdapter<>(LoggedInHomeActivity.this, android.R.layout.simple_spinner_dropdown_item, items);
+                        AnswerSelect.setAdapter(adapter);
+
+                        questions = database.getReference(quizName.toString());
+                        questions.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
+                                    Question tempQuestion = singleSnapshot.getValue(Question.class);
+                                    textQuestion.setText(tempQuestion.getQuestion());
+                                    textAnswerA.setText(tempQuestion.getAnswerA());
+                                    textAnswerB.setText(tempQuestion.getAnswerB());
+                                    textAnswerC.setText(tempQuestion.getAnswerC());
+                                    textAnswerD.setText(tempQuestion.getAnswerD());
+                                    //((ViewGroup)active_quiz_layout.getParent()).removeView(active_quiz_layout);
+                                    //activeQuizDialog.show();//Skips all questions but the last one
+
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+
+
+                        activeQuizDialog.setNegativeButton("Close", new AlertDialog.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int which) {
+                                dialogInterface.dismiss();
+                            }
+                        });
+
+                        activeQuizDialog.setPositiveButton("Submit", new AlertDialog.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int which) {
+                                questions = database.getReference(quizName.toString());
+                                questions.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        // for(DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+                                        //     Question tempQuestion = singleSnapshot.getValue(Question.class);
+                                        //     textQuestion.setText(tempQuestion.getQuestion()); //not doing what I want right now
+                                    }
+
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {
+
+                                    }
+                                });
+                            }
+                        });
+                        activeQuizDialog.show();
+                    }
+                    }
                 }
-            }
+
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
@@ -113,6 +214,10 @@ public class LoggedInHomeActivity extends AppCompatActivity {
         });
     }
 
+
+
+
+    //ADD QUIZ DIALOGUE
     private void displayAddQuizDialogue(){
         AlertDialog.Builder alertdialog = new AlertDialog.Builder(LoggedInHomeActivity.this);
         alertdialog.setTitle("Add Quiz");
@@ -143,25 +248,27 @@ public class LoggedInHomeActivity extends AppCompatActivity {
                 quizzes.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
+                        //Check if quiz name is already taken
                         if (dataSnapshot.child(quiz.getQuizName()).exists()) {
                             Toast.makeText(LoggedInHomeActivity.this, "Quiz name is taken", Toast.LENGTH_SHORT).show();
-                        } else {
+                        }
+                        //Sets quizCreated and quizCreatedInt to required states to run addQuestionDialogue
+                        else {
                             quizCreated = true;
                             quizCreatedInt = 1;
                             quizzes.child(quiz.getQuizName()).setValue(quiz);
                             displayAddQuestionDialogue();
                             Toast.makeText(LoggedInHomeActivity.this, "Quiz creation successful", Toast.LENGTH_SHORT).show();
+                            //resets quizCreated and quizCreatedInt to allow creation of multiple quizzes subsequently
                             quizCreated = false;
                             quizCreatedInt = 0;
-
-
                         }
                         /*if (quizCreated = true){
                             AlertDialog.Builder alertdialog2 = new AlertDialog.Builder(LoggedInHomeActivity.this);
                             alertdialog2.setTitle("Add Question");
                             alertdialog2.setMessage("Please provide the necessary information");
 
-                            Context context = null; //FIX THIS NOOOOOOOOOOOOOOOOOOOOOOOOOOOWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+                            Context context = null; //
                             LayoutInflater inflater2 = LayoutInflater.from(LoggedInHomeActivity.this);//(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE); //this.getLayourInflater();
                             View add_question_layout = inflater2.inflate(R.layout.add_question_layout, null);
                             alertdialog2.setView(add_question_layout);
@@ -258,13 +365,13 @@ public class LoggedInHomeActivity extends AppCompatActivity {
 
 
 
-        alertdialog2.setNegativeButton("Cancel", new AlertDialog.OnClickListener() {
+        alertdialog2.setNegativeButton("Finish", new AlertDialog.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 dialogInterface.dismiss();
             }
         });
-        alertdialog2.setPositiveButton("Submit", new AlertDialog.OnClickListener() {
+        alertdialog2.setPositiveButton("Submit another question", new AlertDialog.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int i) {
                 final Question question = new Question(
@@ -273,7 +380,7 @@ public class LoggedInHomeActivity extends AppCompatActivity {
                     edtNewAnswerB.getText().toString(),
                     edtNewAnswerC.getText().toString(),
                     edtNewAnswerD.getText().toString(),
-                    correctAnswer = "Answer" + CorrectDropdown.getSelectedItem().toString()); //https://stackoverflow.com/questions/10331854/how-to-get-spinner-selected-item-value-to-string
+                    correctAnswer = CorrectDropdown.getSelectedItem().toString()); //https://stackoverflow.com/questions/10331854/how-to-get-spinner-selected-item-value-to-string
                 quizQuestions = database.getReference(edtNewQuizName.getText().toString());
                 quizQuestions.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
@@ -298,5 +405,7 @@ public class LoggedInHomeActivity extends AppCompatActivity {
         });
         alertdialog2.show();
     }
+
+
 
 }
